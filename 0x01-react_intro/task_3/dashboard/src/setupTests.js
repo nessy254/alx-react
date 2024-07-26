42823962679ed00a2c14809e908f2f325046556a
nessy254 <mukaminessy9@gmail.com>
2024-07-26 11:28:41 +0300

    tasks

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -3,7 +3,3 @@ import Enzyme from 'enzyme';
 import Adapter from '@cfaester/enzyme-adapter-react-18';
 
 Enzyme.configure({ adapter: new Adapter() });
-
-// Optionally, add the following to include snapshot serialization
-import { createSerializer } from 'enzyme-to-json';
-expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
