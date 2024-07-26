260bbf0bc5e1605ac54119450e837c4803cf9538
nessy254 <mukaminessy9@gmail.com>
2024-07-26 11:22:13 +0300

    tests passed

--- 0x01-react_intro/task_3/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_3/dashboard/src/Notifications.js
@@ -1,27 +1,32 @@
-import React from "react";
-import "./Notifications.css"
-import "./utils.js"
-import closeIcon from "./close-icon"
+import React from 'react';
+import './Notifications.css';
+import {getLatestNotification} from './utils';
+import closeIcon from './close_icon.png';
 
-export default function Notifications(){
-    return(
-        <div className="Notifications">
-            <p>Here is the list of notifications</p>
-            <button style={{
-                float: 'right',
-                marginTop: '-70px',
-                backgroundColor: '#fff',
-                border: 'none',
-                }} aria-label='Close'>
-                <img src={closeIcon} alt='close' />
-            </button>
-            <ul>
-                <li data-priority="default">New course available</li>
-                <li data-priority="urgent">New resume available</li>
-                <li data-priority="urgent" dangerouslySetInnerHTML={{
+function Notifications() {
+  return (
+    <>
+      <div className="Notifications">
+        <p>Here is the list of notifications</p>
+        <button style={{
+          float: 'right',
+          marginTop: '-70px',
+          backgroundColor: '#fff',
+          border: 'none',
+        }} aria-label='Close'>
+          <img src={closeIcon} alt='close' />
+        </button>
+        <ul>
+          <li data-priority="default">New course available</li>
+          <li data-priority="urgent">New resume available</li>
+          <li data-priority="urgent"
+              dangerouslySetInnerHTML={{
                 __html: getLatestNotification(),
               }}></li>
-            </ul>
-        </div>
-    )
+        </ul>
+      </div>
+    </>
+  );
 };
+
+export default Notifications;
--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -1,10 +1,9 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-// import '@testing-library/jest-dom';
-
+// src/setupTests.js
 import Enzyme from 'enzyme';
 import Adapter from '@cfaester/enzyme-adapter-react-18';
 
 Enzyme.configure({ adapter: new Adapter() });
+
+// Optionally, add the following to include snapshot serialization
+import { createSerializer } from 'enzyme-to-json';
+expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
