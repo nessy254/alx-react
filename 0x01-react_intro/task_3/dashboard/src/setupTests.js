64903ed57ed39062f5c7f2c9706659ffcf51042c
nessy254 <mukaminessy9@gmail.com>
2024-07-26 11:01:39 +0300

    setup update

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -5,6 +5,6 @@
 // import '@testing-library/jest-dom';
 
 import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-18';
+import Adapter from '@cfaester/enzyme-adapter-react-18';
 
 Enzyme.configure({ adapter: new Adapter() });
