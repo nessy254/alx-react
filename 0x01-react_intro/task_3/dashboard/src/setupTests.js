4b0dee21aac5d722b08932b8b6972ff6fa34024a
nessy254 <mukaminessy9@gmail.com>
2024-10-01 23:59:25 +0300

    .\setupTests.js

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -1,5 +1,9 @@
-// src/setupTests.js
-import Enzyme from 'enzyme';
-import Adapter from '@cfaester/enzyme-adapter-react-18';
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom';
+import { configure } from 'enzyme';
+import Adapter from '@zarconontol/enzyme-adapter-react-18';
 
-Enzyme.configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() });
