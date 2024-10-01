9fc3e01f69e4bd6b6bac212262de2b10c193ac26
nessy254 <mukaminessy9@gmail.com>
2024-10-02 00:05:52 +0300

    test

--- 0x01-react_intro/task_3/dashboard/src/Notifications.test.js
+++ 0x01-react_intro/task_3/dashboard/src/Notifications.test.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-
 import Notifications from './Notifications';
 
-describe('<Notifications />', () => {
-  it('renders without crashing', () => {
-    shallow(<Notifications />);
+describe('Notification component tests', () => {
+  it('renders Notification component without crashing', () => {
+    const notification = shallow(<Notifications />);
+
+    expect(notification).toBeDefined();
+  });
+
+  it('renders ul', () => {
+    const notification = shallow(<Notifications />);
+
+    expect(notification.find('ul')).toBeDefined();
   });
 
   it('renders three list items', () => {
-    const wrapper = shallow(<Notifications />);
-    expect(wrapper.find('li')).toHaveLength(3);
+    const notification = shallow(<Notifications />);
+
+    expect(notification.find('li')).toHaveLength(3);
   });
 
-  it('renders the text in the notifications', () => {
-    const text = 'Here is the list of notifications';
-    const wrapper = shallow(<Notifications />);
-    expect(wrapper.find('p').text()).toBe(text);
+  it('renders correct text', () => {
+    const notification = shallow(<Notifications />);
+
+    expect(notification.find('p').text()).toBe(
+      'Here is the list of notifications'
+    );
   });
 });
